de31fb9ce701b559ca987442fe2b361e51ae7a58
chithanh0246 <nguyenchithanhwiko@gmail.com>
2021-07-12 11:33:11 +0700

    tuan 3 p2

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -22,9 +22,10 @@ namespace Lab04.Controllers
         {
             var viewModel = new CourseViewModel
             {
-                Categories = _dbContext.Categories.ToList()
+                Categories = _dbContext.Categories.ToList(),
+                Heading="Add Course"
             };
-            return View(viewModel);
+            return View("CourseForm",viewModel);
         }
         [Authorize]
         [HttpPost]
@@ -34,7 +35,7 @@ namespace Lab04.Controllers
             if(!ModelState.IsValid)
             {
                 viewModel.Categories = _dbContext.Categories.ToList();
-                return View("Create", viewModel);
+                return View("CoureForm", viewModel);
             }
             var course = new Course
             {
@@ -63,6 +64,52 @@ namespace Lab04.Controllers
             };
             return View(viewModel);
                 
+        }
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var courses = _dbContext.Courses
+                .Where(c => c.LecturerId == userId && c.DateTime > DateTime.Now)
+                .Include(l => l.Lecturer)
+                .Include(c => c.Category)
+                .ToList();
+            return View(courses);
+        }
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var course = _dbContext.Courses.Single(c => c.Id == id && c.LecturerId == userId);
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList(),
+                Date = course.DateTime.ToString("dd/M/yyyy"),
+                Time = course.DateTime.ToString("HH:mm"),
+                Category = course.CategoryId,
+                Place = course.Place,
+                Heading = "Edit Course",
+                Id=course.Id
+            };
+            return View("CoureForm", viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(CourseViewModel viewModel) {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("CoureForm", viewModel);
+            }
+            var userId = User.Identity.GetUserId();
+            var course = _dbContext.Courses.Single(c => c.Id == viewModel.Id && c.LecturerId == userId);
+            course.Place = viewModel.Place;
+            course.DateTime = viewModel.GetDateTime();
+            course.CategoryId = viewModel.Category;
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
+
         }
     }
 }
